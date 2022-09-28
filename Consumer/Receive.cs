a7301202704361e075936141f0ff683e3a62d98b
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2022-09-28 12:41:40 -0500

    First Commit

---
+++ Consumer/Receive.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using MassTransit.Model;
+using MassTransit.Testing;
+using static MassTransit.MessageHeaders;
+
+class Receive
+{
+    static async Task Main(string[] args)
+    {
+        var bus = Bus.Factory.CreateUsingRabbitMq(config =>
+        {
+            config.Host(new Uri("amqp://guest:guest@localhost"), c => { });
+
+            //config.ReceiveEndpoint()
+            config.ReceiveEndpoint("order-queue", endpoint =>
+            {
+                
+                endpoint.Handler<Order>(context =>
+                {
+                    return Console.Out.WriteLineAsync($"{context.Message.Name}");
+                });
+            });
+        });
+
+
+        bus.Start();
+
+        Console.WriteLine("Receive listening for messages");
+
+        Console.ReadLine();
+
+        bus.Stop();
+    }
+}
---
+++ ProducerApp/Send.cs
@@ -0,0 +1,55 @@
+﻿using MassTransit.Testing;
+using MassTransit;
+using MassTransit.Model;
+using Bogus;
+
+class Send
+{
+    private readonly ISendEndpointProvider sendEndpoint;
+
+    public Send(ISendEndpointProvider sendEndpoint)
+    {
+        this.sendEndpoint = sendEndpoint;
+    }
+
+    static async Task Main(string[] args)
+    {
+
+        string quitChar = string.Empty;
+        int numberOfRows = 0;
+        while(quitChar != "Q")
+        {
+
+            Console.WriteLine("Enter the number of names you want to display: ");
+            quitChar = Console.ReadLine().ToUpper().Trim();            
+            numberOfRows = Int32.Parse(quitChar);
+                
+            var bus = Bus.Factory.CreateUsingRabbitMq(config =>
+            {
+                config.Host(new Uri("amqp://guest:guest@localhost"), c =>
+                {
+
+                });
+            });
+            var endpoint = await bus.GetSendEndpoint(new Uri("queue:order-queue"));
+            var faker = new Faker("fr");
+            String name = String.Empty;
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                name = faker.Name.FullName() ;
+                Order order = new Order() { Name = name };
+
+                await endpoint.Send(order);
+                Console.WriteLine("Published message: " + name);
+            }
+                
+
+            
+
+
+           
+        }
+
+    }
+}
